01f3350d27eacc929ee072014c0731716ff6bb35
ProRock <uprorock@ya.ru>
2016-10-12 00:51:16 +0300

    onClick helloworld

--- app/src/main/java/com/example/prorock/naturemusic/MainActivity.java
+++ app/src/main/java/com/example/prorock/naturemusic/MainActivity.java
@@ -2,6 +2,10 @@ package com.example.prorock.naturemusic;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button helloButton = (Button) findViewById(R.id.helloButton);
+
+    }
+
+    public void onMyButtonClick(View v)
+    {
+        //Toast.makeText(MainActivity.this, "Hello World", Toast.LENGTH_SHORT).show();
+
+        TextView helloText = (TextView)findViewById(R.id.textView1);
+        helloText.setText("Make your own kind of music");
     }
 }
